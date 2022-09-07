17b19f57909a539010ae6b7fbf1287bbdbdc16d1
zelalem shiferaw <zelalemshiferaw71921@gmail.com>
2022-09-07 21:29:56 +0300

    test file update

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,6 @@
 import os
 import sys
-# sys.path.append("../Pharmaceutical-Sales-prediction/")
-sys.path.append(os.path.abspath(os.path.join("../Pharmaceutical-Sales-prediction/")))
-
-sys.path.append('../scripts')
+sys.path.append("../Pharmaceutical-Sales-prediction/")
 
 from scripts.logger import logger
 logger.info("Pharmaceutical-Sales-prediction Logger")
