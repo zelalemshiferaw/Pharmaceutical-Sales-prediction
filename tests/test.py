efe3081e6663ba99e92170cd038d691fec8198a2
zelalem shiferaw <zelalemshiferaw71921@gmail.com>
2022-09-10 10:29:29 +0300

    Update cml

--- tests/test.py
+++ tests/test.py
@@ -1,20 +1,12 @@
 import unittest
+import pandas as pd
 import sys
 import os
-from pprint import pprint
-import json
-sys.path.append('../')
-from scripts.read_data import ReadData
 
-class TestReadData(unittest.TestCase):
+sys.path.append(os.path.abspath(os.path.join("../Pharmaceutical-Sales-prediction/")))
 
-    def setUp(self) -> pd.DataFrame:
-        self.loader = ReadData()
+from scripts import clean_data
 
-    def test_read_csv(self):
-        ad_df = self.loader.read_csv('tests/test_dataset.csv')
-        self.assertEqual(len(ad_df), 6)
-
-
-if __name__ == '__main__':
-    unittest.main()
+class TestDataClean(unittest.TestCase):
+    def setUp(self) -> None:
+        self.cleaner = clean_data.drop_duplicates()
